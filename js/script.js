e1ed5480104336cd6abffc122b35e58d09c806c4
siljeangelvik <angelviksilje@gmail.com>
2022-01-31 21:25:53 +0100

    oppgave 8 Rofl..

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// noinspection JSValidateTypes
 const cats = [
     {
         name: "Blob",
@@ -12,3 +13,74 @@ const cats = [
     }
 ];
 
+// QUESTION 1
+console.log("QUESTION 1");
+const cat = {
+    complain: "Meow!",
+    whatDoesTheCatSay: function () {
+        return this.complain;
+    }
+};
+console.log(cat.whatDoesTheCatSay());
+
+// QUESTION 2
+console.log("QUESTION 2");
+let heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+console.log(heading);
+
+// QUESTION 3
+console.log("QUESTION 3");
+heading.style.fontSize = "2em";
+console.log(heading.style);
+
+// QUESTION 4
+console.log("QUESTION 4");
+heading.classList.add("subheading");
+console.log(heading.classList);
+
+// QUESTION 5
+console.log("QUESTION 5");
+let paragraphs = document.querySelectorAll("p");
+paragraphs.forEach((paragraphs) => {
+    paragraphs.style.color = "red";
+});
+console.log(paragraphs);
+
+// QUESTION 6
+console.log("QUESTION 6");
+let resultsContainer = document.getElementsByClassName("results")[0];
+
+resultsContainer.innerHTML = `<p>New paragraph</p>`;
+resultsContainer.style.backgroundColor = "yellow";
+
+console.log(resultsContainer);
+
+// QUESTION 7
+console.log("QUESTION 7");
+
+function myFunctionWithTheCats(list) {
+    list.forEach(cat => console.log(cat.name))
+}
+
+myFunctionWithTheCats(cats);
+
+// QUESTION 8
+console.log("QUESTION 8");
+
+function createCats(cats) {
+    cats.forEach((cat) => {
+        let catBox = document.createElement("div");
+        catBox.innerHTML = `<h5> ${cat.name}</h5>`;
+        catBox.innerHTML = `<p> ${cat.age}</p>`;
+
+        if (onload) {
+            catBox.innerHTML = `<p>${cat.age}: Unknown</p>`;
+        }
+        return catBox.innerHTML;
+    })
+}
+
+createCats(cats);
+document.getElementsByClassName("cat-container")[0].innerHTML = createCats;
+console.log(cats);
